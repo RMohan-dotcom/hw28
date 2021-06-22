ba1d2a36e03eb238d9e4290e12068e39b2d615b0
RMohan-dotcom <84121528+RMohan-dotcom@users.noreply.github.com>
2021-06-22 18:07:42 +0530

    Add files via upload

---
+++ js/lau.js
@@ -0,0 +1,39 @@
+class launcher{
+
+constructor(body,anchor){
+    var opt={
+        bodyA:body,
+        pointB:anchor,
+        stiffness:0.004,
+        length:1
+    }
+    this.bodyA=body
+    this.pointB=anchor
+    this.launch=Constraint.create(opt)
+    World.add(world,this.launch)
+}
+attach(body){
+this.launch.bodyA=body
+}
+fly(){
+    this.launch.bodyA=null;
+}
+display(){
+if(this.launch.bodyA){
+var pointA=this.bodyA.position
+var pointB=this.pointB
+strokeWeight(2)
+line(pointA.x,pointA.y,pointB.x,pointB.y)
+
+
+}
+
+}
+
+
+
+
+
+
+
+}
